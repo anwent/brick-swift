6d1ab180ef514a0b78e8d9082b8a668004645796
zhihao.zhang <zhihao.zhang@gotechcn.cn>
2021-06-25 15:37:11 +0800

    init pod

---
+++ Example/Pods/Target Support Files/Pods-brick-swift_Tests/Pods-brick-swift_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_brick_swift_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_brick_swift_Tests
+@end
---
+++ Example/Pods/Target Support Files/brick-swift/brick-swift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_brick_swift : NSObject
+@end
+@implementation PodsDummy_brick_swift
+@end
